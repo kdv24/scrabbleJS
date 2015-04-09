1d31325500a29cb702cccaca048394cbde15eeb1
Bryan Borgeson & Kelly de Vries <student@epicodus.com>
2015-04-09 12:05:20 -0700

    add logic to make words of multiple characters pass

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,46 @@
 function scrabble(word) {
 
-
-
     var values = {
         '1' : ['a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't'],
         '2' : ['d', 'g']
     }
 
+    var arrayWord = word.split('');
+
     var total = 0;
 
-    console.log(word);
 
-    for (var key in values) {
-        if (values.hasOwnProperty(key)) {
-            var score = values[key];
-            for (var letter in score) {
-                if (word === score[letter]) {
-                    total += 1;
+    // For each letter in our word array...
+    for (var wordLetter in arrayWord) {
+        // for each property in values object...
+        for (var key in values) {
+            // if that property exists...
+            if (values.hasOwnProperty(key)) {
+                // rename that key to variable score (for ease of code)...
+                var score = values[key];
+                // for each character in the array associated with the key (or score)...
+                for (var letter in score) {
+                    //
+                    if ((key === '1') && (arrayWord[wordLetter] === score[letter])) {
+                        total += 1;
+                    } else if ((key === '2') && (arrayWord[wordLetter] === score[letter])) {
+                        total += 2;
+                    } else {
+                        total += 0;
+                    }
+
                 }
             }
         }
     }
 
-    console.log(values);
-    console.log(total);
 
     return total;
 }
+
+
+// switch (values) {
+//
+//     case (word === score[letter]):
+//         total+=1;
+//         break;
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,13 @@
 describe("scrabble", function() {
-    it("will return a score of '1' for the 'a' submitted by the user", function() {
+    it("will return a score of '1' for the word 'a' submitted by the user", function() {
         expect(scrabble('a')).to.equal(1);
+    });
+
+    it("will return a score of '2' for the word 'd' submitted by the user", function() {
+        expect(scrabble('d')).to.equal(2);
+    });
+
+    it("will return a score of '2' for the word 'at'", function() {
+        expect(scrabble('at')).to.equal(2);
     });
 });
